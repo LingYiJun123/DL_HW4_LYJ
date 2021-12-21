7192f58ab9600f297292c038c59fe0e1c22ad846
LingYiJun123 <93124374+LingYiJun123@users.noreply.github.com>
2021-12-21 22:07:47 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -6,7 +6,7 @@ from torchvision.models.vgg import vgg16
 class GeneratorLoss(nn.Module):
     def __init__(self):
         super(GeneratorLoss, self).__init__()
-        vgg = vgg16(pretrained=True)
+        vgg = vgg16(pretrained=False)
         loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()
         for param in loss_network.parameters():
             param.requires_grad = False
